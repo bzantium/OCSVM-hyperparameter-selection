1c768012d0478d59e053a2e273d417be96d9cd62
Bricoler <ryumin93@gmail.com>
2020-01-03 00:17:39 +0900

    update

--- algorithm.py
+++ algorithm.py
@@ -1,55 +1,91 @@
 from sklearn.neighbors import NearestNeighbors
+from sklearn.manifold import TSNE
+from matplotlib import pyplot as plt
 import numpy as np
 import math
 
 
-def edge_pattern_detection(X, k=None, threshold=0.01):
-    edge_index = []
-    normal_vector = []
-    if k is None:
-        k = math.ceil(5 * math.log10(len(X)))
-    nbrs = NearestNeighbors(n_neighbors=k+1).fit(X)
-    distances, neighbor_indices = nbrs.kneighbors(X)
-    for i in range(len(X)):
-        neighbors = X[neighbor_indices[i][1:]]
-        X_i = np.tile(X[i], (k, 1))
-        v = (X_i - neighbors) / (np.linalg.norm(X_i - neighbors, axis=1, keepdims=True) + 1e-8)
-        n = np.sum(v, axis=0, keepdims=True)
-        normal_vector.append(n.squeeze())
-        theta = np.dot(v, n.transpose())
-        l = np.mean(theta >= 0)
-        if l > 1 - threshold:
-            edge_index.append(i)
-    normal_vector = np.array(normal_vector)
-    return edge_index, distances, neighbor_indices, normal_vector
+class SelfAdaptiveShifting:
+    def __init__(self, data):
+        self.data = data
+        self.k = math.ceil(5 * math.log10(len(self.data)))
+        self.distances = None
+        self.all_neighbor_indices = None
+        self.edge_indice = None
+        self.normal_vectors = None
+        self.pseudo_outliers = None
+        self.pseudo_targets = None
 
+    def edge_pattern_detection(self, threshold=0.01):
+        edge_indice, normal_vectors = [], []
+        nbrs = NearestNeighbors(n_neighbors=self.k + 1).fit(self.data)
+        self.distances, self.all_neighbor_indices = nbrs.kneighbors(self.data)
+        for i in range(len(self.data)):
+            neighbors = self.data[self.all_neighbor_indices[i][1:]]
+            x_i = np.tile(self.data[i], (self.k, 1))
+            v = (x_i - neighbors) / (np.expand_dims(self.distances[i][1:], axis=1) + 1e-8)
+            n = np.sum(v, axis=0, keepdims=True)
+            normal_vectors.append(n.squeeze())
+            theta = np.dot(v, n.transpose())
+            l = np.mean(theta >= 0)
+            if l > 1 - threshold:
+                edge_indice.append(i)
+        self.normal_vectors = np.array(normal_vectors)
+        self.edge_indice = edge_indice
 
-def generate_pseudo_outlier(X_edge, ns_magnitude, edge_normal_vector):
-    pseudo_outlier_X = X_edge + edge_normal_vector / \
-                       (np.linalg.norm(edge_normal_vector, axis=1, keepdims=True) + 1e-8) * ns_magnitude
+    def generate_pseudo_outliers(self):
+        edge_data = self.data[self.edge_indice]
+        l_ns = np.mean(self.distances[self.edge_indice][1:])
+        edge_normal_vectors = self.normal_vectors[self.edge_indice]
+        self.pseudo_outliers = edge_data + edge_normal_vectors / (np.linalg.norm(edge_normal_vectors, axis=1, keepdims=True) + 1e-8) * l_ns
+        return self.pseudo_outliers
 
-    peeudo_outlier_y = -np.ones(len(pseudo_outlier_X))
-    return pseudo_outlier_X, peeudo_outlier_y
+    def generate_pseudo_targets(self):
+        shift_directions = -self.normal_vectors
+        unit_shift_directions = shift_directions / (np.linalg.norm(shift_directions, axis=1, keepdims=True) + 1e-8)
+        pseudo_targets = []
+        for i in range(len(self.data)):
+            min_product = self.get_product_minimum(self.data[i], unit_shift_directions[i], self.all_neighbor_indices[i])
+            pseudo_targets.append(self.data[i] + min_product * unit_shift_directions[i])
+            pseudo_targets.append(self.data[i] - min_product * unit_shift_directions[i])
+        self.pseudo_targets = np.array(pseudo_targets)
+        return self.pseudo_targets
 
+    def get_product_minimum(self, x_i, unit_shift_direction, neighbor_indices):
+        x_ij_min = x_i
+        min_product = float("inf")
+        for neighbor_index in neighbor_indices[1:]:
+            x_ij = self.data[neighbor_index]
+            inp = np.dot(unit_shift_direction, x_ij - x_i)
+            if 0 < inp < min_product:
+                min_product = inp
+                x_ij_min = x_ij
+        if x_ij_min is x_i:
+            min_product = 0
+        return min_product
 
-def generate_pseudo_target(X, normal_vector, neighbor_indices):
-    pseudo_target_X = []
-    for i in range(len(X)):
-        x_ij_min = get_product_minimum(X[i], normal_vector[i], X[neighbor_indices[i][1:]])
-        noramalized_direction = normal_vector[i] / (np.linalg.norm(normal_vector[i]) + 1e-8)
-        pseudo_target_X.append(X[i] + np.dot(noramalized_direction, x_ij_min - X[i]) * noramalized_direction)
-    pseudo_target_X = np.array(pseudo_target_X)
-    peeudo_target_y = np.ones(len(pseudo_target_X))
-    return pseudo_target_X, peeudo_target_y
+    def visualize(self):
+        plt.figure()
+        if np.size(self.data, 1) > 2:
+            all_data = np.concatenate((self.data, self.pseudo_outliers, self.pseudo_targets), axis=0)
+            tsne_all_data = TSNE(n_components=2, random_state=42).fit_transform(all_data)
+            tsne_data = tsne_all_data[:len(self.data)]
+            tsne_pseudo_outliers = tsne_all_data[len(self.data):(len(self.data) + len(self.pseudo_outliers))]
+            tsne_pseudo_targets = tsne_all_data[-len(self.pseudo_targets):]
 
+            origin = plt.scatter(tsne_data[:, 0], tsne_data[:, 1], c='k', lw=0)
+            edge = plt.scatter(tsne_data[self.edge_indice][:,0], tsne_data[self.edge_indice][:, 1], c = 'b', lw=0)
+            outlier = plt.scatter(tsne_pseudo_outliers[:, 0], tsne_pseudo_outliers[:, 1], c='r', lw=0)
+            target = plt.scatter(tsne_pseudo_targets[:, 0], tsne_pseudo_targets[:, 1], c='g', lw=0)
+        else:
+            origin = plt.scatter(self.data[:, 0], self.data[:, 1], c='k', lw=0)
+            edge = plt.scatter(self.data[self.edge_indice][:,0], self.data[self.edge_indice][:, 1], c = 'b', lw=0)
+            outlier = plt.scatter(self.pseudo_outliers[:, 0], self.pseudo_outliers[:, 1], c='r', lw=0)
+            target = plt.scatter(self.pseudo_targets[:, 0], self.pseudo_targets[:, 1], c='g', lw=0)
 
-def get_product_minimum(x_i, normal_vector_i, neighbors_i):
-    normalized_normal_vector = normal_vector_i / (np.linalg.norm(normal_vector_i) + 1e-8)
-    min_value = float("inf")
-    x_ij_min = x_i
-    for x_ij in neighbors_i:
-        inp = np.dot(normalized_normal_vector, x_ij - x_i)
-        if 0 < inp < min_value:
-            min_value = inp
-            x_ij_min = x_ij
-    return x_ij_min
+        plt.legend((origin, edge, outlier, target),
+           ('Target data', 'Edge', 'Pseudo outliers', 'Pseudo target data'),
+           loc='upper left',
+           ncol=1,
+           fontsize=12)
+        plt.show()
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from sklearn.model_selection import train_test_split
 from sklearn.svm import OneClassSVM
 from sklearn.metrics import f1_score, matthews_corrcoef, accuracy_score
-from utils import *
-from algorithm import *
+from sklearn.preprocessing.data import MinMaxScaler
+from utils import load_data
+from algorithm import SelfAdaptiveShifting
 import numpy as np
 import random
 import argparse
@@ -13,6 +14,7 @@ def parse_arguments():
     # argument parsing
     parser = argparse.ArgumentParser(description="Specify Params for Experimental Setting")
     parser.add_argument('--data', type=str, default='fourclass')
+    parser.add_argument('--scale', default=False, action='store_true')
     parser.add_argument('--kernel', default='rbf')
     parser.add_argument('--seed', type=int, default=42)
     parser.add_argument('--threshold', type=float, default=0.01)
@@ -23,6 +25,9 @@ def main():
     args = parse_arguments()
     random.seed(args.seed)
     X, y = load_data(args)
+    if args.scale:
+        scaler = MinMaxScaler()
+        X = scaler.fit_transform(X)
 
     y_value = np.unique(y)
 
@@ -34,24 +39,24 @@ def main():
     target_X_train, target_X_test, target_y_train, target_y_test = train_test_split(target_X, target_y, shuffle=True,
                                                                                     random_state=args.seed, test_size=1/3)
 
-    edge_index, distances, neighbor_indices, normal_vector = edge_pattern_detection(target_X_train,
-                                                                                    threshold=args.threshold)
-    ns_magnitude = np.mean(distances[edge_index, 1:])
-    pseudo_outlier_X, pseudo_outlier_y = generate_pseudo_outlier(target_X_train[edge_index],
-                                                                 ns_magnitude, normal_vector[edge_index])
-    pseudo_target_X, pseudo_target_y = generate_pseudo_target(target_X_train, normal_vector, neighbor_indices)
+    self_adaptive_shifting = SelfAdaptiveShifting(target_X_train)
+    self_adaptive_shifting.edge_pattern_detection(args.threshold)
+    pseudo_outlier_X = self_adaptive_shifting.generate_pseudo_outliers()
+    pseudo_target_X = self_adaptive_shifting.generate_pseudo_targets()
+    pseudo_outlier_y = -np.ones(len(pseudo_outlier_X))
+    pseudo_target_y = np.ones(len(pseudo_target_X))
 
-    gamma_candidates = [1e-4, 1e-3, 1e-2, 1e-1, 1e-0, 1e+1, 1e+2, 1e+3, 1e+4]
-    nu_candidates = [0.001, 0.005, 0.01, 0.05, 0.1]
+    gamma_candidates = [1e-4, 1e-3, 1e-2, 1e-1, 1e-0, 1e+1, 1e+2, 1e+3, 1/np.size(target_X, -1)]
+    nu_candidates = [0.005, 0.01, 0.05, 0.1, 0.5]
 
     best_err = 1.0
-    best_gamma, best_nu = None, None
+    best_gamma, best_nu = 1/np.size(target_X, -1), 0.5
     for gamma in tqdm(gamma_candidates):
         for nu in tqdm(nu_candidates):
             model = OneClassSVM(gamma=gamma, nu=nu).fit(target_X_train)
             err_o = 1 - np.mean(model.predict(pseudo_outlier_X) == pseudo_outlier_y)
             err_t = 1 - np.mean(model.predict(pseudo_target_X) == pseudo_target_y)
-            err = (err_o + err_t) / 2
+            err = float((err_o + err_t) / 2)
             if err < best_err:
                 best_err = err
                 best_gamma = gamma
@@ -62,11 +67,24 @@ def main():
     outlier_pred = best_model.predict(outlier_X)
     y_pred = np.concatenate((target_pred, outlier_pred))
     y_true = np.concatenate((target_y_test, outlier_y))
-    f1 = f1_score(y_true, y_pred, average="micro")
+    f1 = f1_score(y_true, y_pred, average="binary")
     mcc = matthews_corrcoef(y_true, y_pred)
     acc = accuracy_score(y_true, y_pred)
     print("\n[%s] (gamma: %.4f, nu: %.4f, err: %.4f) \nf1-score: %.4f, mcc: %.4f, acc: %.4f"
           % (args.data, best_gamma, best_nu, best_err, f1, mcc, acc))
+
+    model = OneClassSVM(kernel=args.kernel).fit(target_X_train)
+    target_pred = model.predict(target_X_test)
+    outlier_pred = model.predict(outlier_X)
+    y_pred = np.concatenate((target_pred, outlier_pred))
+    y_true = np.concatenate((target_y_test, outlier_y))
+    f1 = f1_score(y_true, y_pred, average="binary")
+    mcc = matthews_corrcoef(y_true, y_pred)
+    acc = accuracy_score(y_true, y_pred)
+    print("\n[%s] (default setting) \nf1-score: %.4f, mcc: %.4f, acc: %.4f"
+          % (args.data, f1, mcc, acc))
+
+    self_adaptive_shifting.visualize()
 
 
 if __name__ == "__main__":
